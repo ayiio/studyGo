41c88e673c3eac4b75cc3e6e40e1e96dba3be5e0
ayiio <61615400+ayiio@users.noreply.github.com>
2023-04-24 23:13:58 +0800

    Update consolLog.go

--- mylog/consolLog.go
+++ mylog/consolLog.go
@@ -1,51 +1,67 @@
 package mylogger
 
+//向终端上写日志
+
 import (
 	"fmt"
-	"strings"
+	"time"
 )
 
-func NewconsoleLogger(s string) consoleLogger {
-	temps := strings.ToLower(s)
-	n := transLevel(temps)
+// mylog 结构体
+type MyConsolelog struct {
+	level logLevel
+}
 
-	return consoleLogger{
-		level: n,
+// NewLogger 构造函数
+func NewConsolelog(s string) MyConsolelog {
+	l, err := parseString(s)
+	if err != nil {
+		panic(err)
+	}
+	return MyConsolelog{
+		level: l,
 	}
 }
 
-func (c consoleLogger) log(level logLevel, format string, a ...interface{}) {
-	if c.enable(level) {
+// a ...interface{} 切片传入
+func (l MyConsolelog) log(loglevel logLevel, format string, a ...interface{}) {
+	if l.enable(loglevel) {
 		msg := fmt.Sprintf(format, a...)
-		fileName, funcName, lineNo := getCaller()
-		fmt.Printf("[%s] [%s] %s:%s:%d %s\n", unTransLevel(level), getTime(), fileName, funcName, lineNo, msg)
+		now := time.Now().Format("2006-01-02 15:04:05.000")
+		fmt.Printf("[%s] [%s] 这是一条日志\n", now, msg)
 	}
 }
 
-func (c consoleLogger) enable(mylevel logLevel) bool {
-	return c.level <= mylevel
+// Debug 函数
+func (l MyConsolelog) Debug(s string) {
+	l.log(DEBUG, "debug")
 }
 
-func (c consoleLogger) Debug(s string, a ...interface{}) {
-	c.log(DebugLevel, s, a...)
+// Info 函数
+func (l MyConsolelog) Info(s string) {
+	l.log(INFO, "debug")
 }
 
-func (c consoleLogger) Info(s string, a ...interface{}) {
-	c.log(InfoLevel, s, a...)
+// Trace 函数
+func (l MyConsolelog) Trace(s string) {
+	l.log(TRACE, "debug")
 }
 
-func (c consoleLogger) Trace(s string, a ...interface{}) {
-	c.log(TraceLevel, s, a...)
+// Warning 函数
+func (l MyConsolelog) Warning(s string) {
+	l.log(WARNING, "debug")
 }
 
-func (c consoleLogger) Warning(s string, a ...interface{}) {
-	c.log(WarningLevel, s, a...)
+func (l MyConsolelog) Error(s string) {
+	l.log(ERROR, "debug")
 }
 
-func (c consoleLogger) Error(s string, a ...interface{}) {
-	c.log(ErrorLevel, s, a...)
+// Fatal 函数
+func (l MyConsolelog) Fatal(s string) {
+	l.log(FATAL, "debug")
 }
 
-func (c consoleLogger) Fatal(s string, a ...interface{}) {
-	c.log(FatalLevel, s, a...)
+func (l MyConsolelog) enable(L logLevel) bool {
+	return l.level <= L
 }
+
