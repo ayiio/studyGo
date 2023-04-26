aa8c76732a03d496d39e25970693cc69b46601f4
ayiio <61615400+ayiio@users.noreply.github.com>
2023-04-27 00:35:40 +0800

    Update Once.go

--- sync/Once.go
+++ sync/Once.go
@@ -25,8 +25,11 @@ func f2(ch1 <-chan int, ch2 chan<- int) {
 		if !ok {
 			break
 		}
-		ch2 <- x * 2
+		ch2 <- x * 2  
 	}
+	//如果计算复杂，或写入ch2前有其他操作，则可能出现问题
+	//第一个goroutine-go f2(a,b)可能已经将ch1取完，在进行到其他操作时第二个goroutine-go f2(a,b)执行到<-ch1，此时ok将为false，第二个goroutine将继续执行close(ch2)
+	//第二个goroutine关闭后退出f2，第一个goroutine执行完其他操作后将进行ch2<-，此时chan已关闭，将报panic。可不使用Once，将close拿到wg.Wait()后执行
 	once.Do(func() { close(ch2) })
 }
 
