fe2420d62e34389425ff31922fea7d485e996a59
ayiio <61615400+ayiio@users.noreply.github.com>
2023-04-26 21:34:46 +0800

    Update client.go

--- net/sticky/client.go
+++ net/sticky/client.go
@@ -1,1 +1,20 @@
+package main
 
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:30000")
+	if err != nil {
+		fmt.Printf("dial failed, err=%v\n", err)
+		return
+	}
+	defer conn.Close()
+	for i := 0; i < 20; i++ {
+		msg := "hello, hello net and tcp"
+		conn.Write([]byte(msg))
+	}
+
+}
