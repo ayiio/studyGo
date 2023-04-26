c128813fb56a950a6cefd14d898857b857052369
ayiio <61615400+ayiio@users.noreply.github.com>
2023-04-26 21:40:38 +0800

    Update server.go

--- net/sticky/server.go
+++ net/sticky/server.go
@@ -1,1 +1,43 @@
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+)
+
+func process(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	var buf [1025]byte
+	for {
+		n, err := reader.Read(buf[:])
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Printf("read from client failed, err=%v\n", err)
+			return
+		}
+		recvStr := string(buf[:n])
+		fmt.Println("收到的消息：", recvStr)
+	}
+}
+
+func main() {
+	listen, err := net.Listen("tcp", "127.0.0.1:30000")
+	if err != nil {
+		fmt.Printf("listen failed, err=%v\n", err)
+		return
+	}
+	defer listen.Close()
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Printf("accept failed, err=%v\n", err)
+			return
+		}
+		go process(conn)
+	}
+}
