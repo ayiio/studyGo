94ec313fd141af503ec321bf2ca7fbdbc1acd40d
ayiio <61615400+ayiio@users.noreply.github.com>
2023-04-30 01:20:34 +0800

    Update mysplit_test.go

--- test/unit_test/mysplit_test.go
+++ test/unit_test/mysplit_test.go
@@ -8,33 +8,33 @@ import (
 // unit test : go test -v
 
 func TestMySplit(t *testing.T) {
-	get := Split("abcdbef", "b")
+	got := Split("abcdbef", "b")
 	want := []string{"a", "cd", "ef"}
-	if !reflect.DeepEqual(get, want) {
-		t.Errorf("failed, want:%v but got:%v\n", want, get)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("failed, want:%v but got:%v\n", want, got)
 	}
 }
 
 func Test2MySplit(t *testing.T) {
-	get := Split("abc:db:e:f", ":")
+	got := Split("abc:db:e:f", ":")
 	want := []string{"abc", "db", "e", "f"}
-	if !reflect.DeepEqual(get, want) {
-		t.Errorf("failed, want:%v but got:%v\n", want, get)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("failed, want:%v but got:%v\n", want, got)
 	}
 }
 
 func Test3MySplit(t *testing.T) {
-	get := Split("abbcdbbef", "bb")
+	got := Split("abbcdbbef", "bb")
 	want := []string{"a", "cd", "ef"}
-	if !reflect.DeepEqual(get, want) {
-		t.Errorf("failed, want:%v but got:%v\n", want, get)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("failed, want:%v but got:%v\n", want, got)
 	}
 }
 
 func Test4MySplit(t *testing.T) {
-	get := Split("打麻将的张三有三张麻将打", "麻将")
+	got := Split("打麻将的张三有三张麻将打", "麻将")
 	want := []string{"打", "的张三有三张", "打"}
-	if !reflect.DeepEqual(get, want) {
-		t.Errorf("failed, want:%v but got:%v\n", want, get)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("failed, want:%v but got:%v\n", want, got)
 	}
 }
