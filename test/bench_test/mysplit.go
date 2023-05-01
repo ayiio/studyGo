b3263376918cb7ac82ef059e088c61d2c8a8653b
ayiio <61615400+ayiio@users.noreply.github.com>
2023-05-01 23:28:16 +0800

    Create mysplit.go

---
+++ test/bench_test/mysplit.go
@@ -0,0 +1,25 @@
+package bt01
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Split 字符串切割
+func Split(str, seq string) (ret []string) {
+	// example: str="abcdbe" seq="b" ret=[]string{"a", "cd", "e"}
+	index := strings.Index(str, seq)
+	for index > -1 {
+		if string(str[:index]) != "" {
+			ret = append(ret, str[:index])
+		}
+		str = str[index+len(seq):]
+		index = strings.Index(str, seq)
+	}
+	ret = append(ret, str)
+	if 1 < 0 {
+		// 验证测试覆盖率
+		fmt.Println("can not run this part")
+	}
+	return
+}
