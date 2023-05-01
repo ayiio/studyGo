e23ea73202f05bf8464f1fe78651a6b0939e0edb
ayiio <61615400+ayiio@users.noreply.github.com>
2023-05-02 00:00:32 +0800

    Create fib_test.go

---
+++ test/bench_test/bench_compare/fib_test.go
@@ -0,0 +1,16 @@
+package bt01
+
+import "testing"
+
+// 比较函数的壳子
+func benchmarkFib(b *testing.B, n int) {
+	for i := 0; i < b.N; i++ {
+		Fib(n)
+	}
+}
+
+func BenchmarkFib1(b *testing.B)  { benchmarkFib(b, 1) }
+func BenchmarkFib2(b *testing.B)  { benchmarkFib(b, 2) }
+func BenchmarkFib3(b *testing.B)  { benchmarkFib(b, 3) }
+func BenchmarkFib10(b *testing.B) { benchmarkFib(b, 10) }
+func BenchmarkFib20(b *testing.B) { benchmarkFib(b, 10) }
