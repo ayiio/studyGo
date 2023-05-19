46ac43cfbb279f045651c263f72e498b5af2085a
ayiio <61615400+ayiio@users.noreply.github.com>
2023-05-19 23:59:36 +0800

    Update taillog.go

--- kafka/logagent/taillog/taillog.go
+++ kafka/logagent/taillog/taillog.go
@@ -1,3 +1,36 @@
 package taillog
 
 // 用于收集日志
+
+import (
+	"time"
+
+	"github.com/hpcloud/tail"
+)
+
+var (
+	tails *tail.Tail
+)
+
+func InitTail(path string) (err error) {
+	config := tail.Config{
+		Location:  &tail.SeekInfo{Offset: 0, Whence: 2},
+		ReOpen:    true,
+		MustExist: false,
+		Follow:    true,
+		Poll:      true,
+	}
+	tails, err = tail.TailFile(path, config)
+	return
+}
+
+func TailFile() (line *tail.Line) {
+	select {
+	case line = <-tails.Lines:
+		return
+	default:
+		time.Sleep(time.Second)
+	}
+	return
+}
+
