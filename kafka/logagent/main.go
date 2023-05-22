5fbac16e4f9a31b36ded2be844ed26955cafa9fd
ayiio <61615400+ayiio@users.noreply.github.com>
2023-05-22 23:11:49 +0800

    Update main.go

--- kafka/logagent/main.go
+++ kafka/logagent/main.go
@@ -5,36 +5,50 @@ import (
 	"logagent/logagent/config"
 	"logagent/logagent/kafka"
 	"logagent/logagent/taillog"
+	"time"
 )
 
 // loagent入口
 
+var (
+	cfg *config.AppConfig
+)
+
+func run() {
+	// 1.读取日志
+	for {
+		select {
+		case line := <-taillog.TailFile():
+			// 2.发送到kafka
+			kafka.SendMsg(cfg.KafkaConfig.Topic, line.Text)
+		default:
+			time.Sleep(time.Second)
+		}
+	}
+}
+
 func main() {
 	// 0.load config.ini
-	param, err := config.InitParam()
-	if err != nil {
-		fmt.Printf("init file load failed, err=%v\n", err)
+	cfg = config.InitConfig()
+	if cfg == nil {
+		fmt.Println("init file load failed")
 		return
 	}
 	fmt.Println("# load init file success.")
 	// 1.初始化kafka连接
-	err = kafka.InitKafka([]string{param.Address})
+	err := kafka.InitKafka([]string{cfg.KafkaConfig.Address})
 	if err != nil {
 		fmt.Printf("init Kafka failed, err=%v\n", err)
 		return
 	}
 	fmt.Println("# init kafka success.")
 	// 2.打开日志文件准备收集日志
-	err = taillog.InitTail(param.Path)
+	err = taillog.InitTail(cfg.TailConfig.FilePath)
 	if err != nil {
 		fmt.Printf("init Tail failed, err=%v\n", err)
 	}
 	fmt.Println("# init tail success.")
-	line := taillog.TailFile()
-	pid, offset, err := kafka.SendMsg(param.Topic, line.Text)
-	if err != nil {
-		fmt.Printf("send msg to kafka failed, err=%v\n", err)
-		return
-	}
-	fmt.Printf("pid=%v, offset=%v\n", pid, offset)
+	// 处理日志
+	run()
 }
+
