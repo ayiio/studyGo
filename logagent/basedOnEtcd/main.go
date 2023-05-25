d64aaffc8106ea8b59b51f3bd5f648f3a130f8db
ayiio <61615400+ayiio@users.noreply.github.com>
2023-05-26 01:05:04 +0800

    Update main.go

--- logagent/basedOnEtcd/main.go
+++ logagent/basedOnEtcd/main.go
@@ -25,7 +25,7 @@ func main() {
 	fmt.Println("# load init file success.")
 
 	// 1.初始化kafka连接
-	err := kafka.InitKafka([]string{cfg.KafkaConfig.Address})
+	err := kafka.InitKafka([]string{cfg.KafkaConfig.Address}, cfg.KafkaConfig.MaxChanSize)
 	if err != nil {
 		fmt.Printf("init Kafka failed, err=%v\n", err)
 		return
@@ -41,7 +41,7 @@ func main() {
 	fmt.Println("# init etcd success.")
 
 	// 2-1.从etcd中拉取日志收集项信息
-	confs, err := etcd.GetConfByKey(cfg.EtcdConfig.LogKey)
+	logEntries, err := etcd.GetConfByKey(cfg.EtcdConfig.LogKey)
 	if err != nil {
 		fmt.Printf("get conf from etcd failed, err=%v\n", err)
 		return
@@ -49,16 +49,9 @@ func main() {
 	fmt.Println("# get conf from etcd success.")
 
 	// 2-2.分配哨兵检测收集项信息的变更，以便随时通知app实现热加载
+	etcd.Watcher(cfg.EtcdConfig.LogKey)
 
-	// 3.收集日志，发往kafka
-	// 3-1.遍历log配置项，创建tailobj
-	for _, logconf := range confs {
-		err = taillog.InitTail(logconf.Path)
-		if err != nil {
-			fmt.Printf("init tail failed, err=%v\n", err)
-			return
-		}
+	// 3.tailf收集日志，发往kafka
+	taillog.Init(logEntries)
 
-	}
-	// 3-2.发往Kafka
 }
