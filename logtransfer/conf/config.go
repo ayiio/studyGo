55e7168dd1a00c0ffe33674e2079411a20e908f4
ayiio <61615400+ayiio@users.noreply.github.com>
2023-06-06 01:34:17 +0800

    Update config.go

--- logtransfer/conf/config.go
+++ logtransfer/conf/config.go
@@ -1,1 +1,36 @@
+package conf
+
+import (
+	"fmt"
+
+	"gopkg.in/ini.v1"
+)
+
+type KafkaConf struct {
+	Address string `ini:"address"`
+	Topic   string `ini:"topic"`
+}
+
+type ESConf struct {
+	Address  string `ini:"address"`
+	ChanSize int    `ini:"chansize"`
+	GoNum    int    `ini:"gonum"`
+	User     string `ini:"user"`
+	Passwd   string `ini:"passwd"`
+}
+
+type LogTransfer struct {
+	KafkaConf `ini:"kafka"`
+	ESConf    `ini:"es"`
+}
+
+func InitConf() *LogTransfer {
+	var lt LogTransfer
+	err := ini.MapTo(&lt, "./conf/config.ini")
+	if err != nil {
+		fmt.Printf("ini map failed, err=%v\n", err)
+		return nil
+	}
+	return &lt
+}
 
