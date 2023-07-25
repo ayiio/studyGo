d199fac99dc7246c846eee3579cec5d1737ba3df
ayiio <61615400+ayiio@users.noreply.github.com>
2023-07-26 00:27:35 +0800

    Create main.go

---
+++ rpc/optionModel/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	NewOptins(SetStringOption1("1"),
+		SetStringOption2("2"),
+		SetStringOption3("3"),
+		SetIntOption1(1),
+		SetIntOption2(2),
+		SetIntOption3(3),
+	)
+}
