d975af781abbe3b5be2cff2237c623087c5b5e0d
ayiio <61615400+ayiio@users.noreply.github.com>
2023-08-01 22:52:57 +0800

    Create registry.go

---
+++ rpc/registercomp/registry.go
@@ -0,0 +1,22 @@
+package registry
+
+import "context"
+
+// Name(): 插件名，例如etcd
+// Init(opts ...Option):初始化，里面用选项设计模式做初始化
+// Register():服务注册
+// Unregister():服务反注册，例如服务器停掉，注册列表销毁
+// GetService():服务发现(ip port[]string)
+
+type Registry interface {
+	//插件名
+	Name() string
+	//初始化
+	Init(ctx context.Context, opts ...Option) (err error)
+	//服务注册
+	Register(ctx context.Context, service *Service) (err error)
+	//服务反注册
+	Unregister(ctx context.Context, service *Service) (err error)
+	//服务发现
+	GetService(ctx context.Context, name string) (service *Service, err error)
+}
